0d98e40ab07a8025c98ef74e0a3ed9ea831f42b8
Camilo Garma <ctgarma@gmail.com>
2018-02-06 22:19:08 +1100

    fixed

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -12,29 +12,3 @@ request(app)
   .expect('Content-Type', /json/)
   .end(done);
 });
-
-it('should return a json with drm equal to true', (done) => {
-request(app)
-  .post('/')
-  .send(testData)
-  .expect(200)
-  .expect('Content-Type', /json/)
-  .expect((res)=>{
-    var arr = res.body.response;
-    var arr = arr.filter(arr => arr.drm != true);
-    expect(arr.length==0);
-  }).end(done);
-});
-
-it('should return a json with episodecount > 0', (done) => {
-request(app)
-  .post('/')
-  .send(testData)
-  .expect(200)
-  .expect('Content-Type', /json/)
-  .expect((res)=>{
-    var arr = res.body.response;
-    var arr = arr.filter(arr => arr.episodeCount <= 0);
-    expect(arr.length==0);
-  }).end(done);
-});
